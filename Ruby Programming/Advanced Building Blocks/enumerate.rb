2b7b5dd8b79ffee67bd1afa52b4bcf4310f7b6eb
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-03-26 23:49:58 +0100

    Finished my_all for every scenario

--- Ruby Programming/Advanced Building Blocks/enumerate.rb
+++ Ruby Programming/Advanced Building Blocks/enumerate.rb
@@ -1,14 +1,14 @@
 module Enumerable
   def my_each
     return to_enum if !block_given?
-    for i in 0..self.size
+    for i in 0..self.size-1
       yield(self[i])
     end
   end
 
   def my_each_with_index
     return to_enum if !block_given?
-    for i in 0..self.size
+    for i in 0..self.size-1
       yield(self[i], i)
     end
   end
@@ -16,25 +16,26 @@ module Enumerable
   def my_select
     return to_enum if !block_given?
     arr = []
-    for i in 0...self.size
+    for i in 0...self.size-1
       arr << self[i] if yield(self[i])
     end
     arr
   end
 
-  def my_all?
-      for i in 0..self.size
-        return true if yield(self[i])
-        return false
+    def my_all?
+      counter = 0
+      for i in 0..self.size-1
+        if !block_given? && (self[i] === false || self[i] === nil)
+          counter += 1
+        end
+        if block_given? && (self[i] == false || self[i] == nil || yield(self[i]) == false || yield(self[i]) == nil)
+          counter += 1
+        end
       end
-  end
-
-
-  def my_any?
-
-  end
+      return true if counter == 0
+      return false if counter > 0
+    end
 
 end
 
-
-[1,2,4].my_each{|n| puts n}
+puts [1,2,false].my_all?
