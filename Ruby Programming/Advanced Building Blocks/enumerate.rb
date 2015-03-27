93573e062ab0369e410241cc2bbef138f41ccf77
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-03-27 16:26:27 +0100

    Finsished advanced building blocks

--- Ruby Programming/Advanced Building Blocks/enumerate.rb
+++ Ruby Programming/Advanced Building Blocks/enumerate.rb
@@ -22,20 +22,78 @@ module Enumerable
     arr
   end
 
-    def my_all?
-      counter = 0
-      for i in 0..self.size-1
-        if !block_given? && (self[i] === false || self[i] === nil)
-          counter += 1
-        end
-        if block_given? && (self[i] == false || self[i] == nil || yield(self[i]) == false || yield(self[i]) == nil)
-          counter += 1
-        end
+  def my_all?
+    counter = 0
+    for i in 0..self.size-1
+      if !block_given? && (self[i] === false || self[i] === nil)
+        counter += 1
       end
-      return true if counter == 0
-      return false if counter > 0
+      if block_given? && (self[i] == false || self[i] == nil || yield(self[i]) == false || yield(self[i]) == nil)
+        counter += 1
+      end
     end
+    return true if counter == 0
+    return false if counter > 0
+  end
+
+  def my_any?
+    counter = 0
+    for i in 0..self.size-1
+      if !block_given? && (self[i] != false || self[i] != false)
+        counter += 1
+      end
+      if block_given? && self[i] != false && self[i] != nil && yield(self[i])
+        counter += 1
+      end
+    end
+    return false if counter == 0
+    return true if counter > 0
+  end
+
+  def my_none?
+    counter = 0
+    for i in 0..self.size-1
+      if !block_given? && (self[i] === false || self[i] === nil)
+        counter += 1
+      end
+      if block_given? && (self[i] == false || self[i] == nil || !yield(self[i]))
+        counter += 1
+      end
+    end
+    return false if counter != self.size
+    return true if counter == self.size
+  end
+
+  def my_count(arg=nil)
+    counter = 0
+    for i in 0..self.size-1
+      if !block_given? && arg==nil
+        counter = self.size
+      elsif block_given?
+        counter += 1 if yield(self[i])
+      else
+        counter += 1 if self[i] == arg
+      end
+    end
+    counter
+  end
+
+  def my_map(&proc)
+    arr = []
+    return to_enum if !block_given?
+    for i in 0..self.size-1
+      arr.push(self[i]) if !proc && yield(self[i])
+      arr.push(self[i]) if proc && proc.call(self[i])
+    end
+    arr
+  end
+
+  def my_inject(arg=0)
+    for i in 0..self.size-1
+      arg = yield(arg, self[i])
+    end
+    arg
+  end
 
 end
 
-puts [1,2,false].my_all?
